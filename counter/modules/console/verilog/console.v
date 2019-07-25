b240ae4050051d4282a23922bf9ce2c73884a9e4
Sameer Puri <sameer@users.noreply.github.com>
2019-07-24 22:52:52 -0700

    add blink support

--- counter/modules/console/verilog/console.v
+++ counter/modules/console/verilog/console.v
@@ -18,24 +18,31 @@ attributemap attributemap(.attribute(attribute), .fgrgb(fgrgb), .bgrgb(bgrgb), .
 reg [9:0] prevcy = 0;
 reg [3:0] vindex = 0;
 reg [2:0] hindex = 0;
+reg [5:0] blink_timer = 0;
+
 always @(posedge CLK_PIXEL)
 begin
     if (cx == 0 && cy == 0)
     begin
         prevcy <= 0;
-        vindex = 0;
-        hindex = 0;
+        vindex <= 0;
+        hindex <= 0;
+        blink_timer <= blink_timer + 1'b1;
     end
     else if (prevcy != cy)
     begin
         prevcy <= cy;
-        vindex = vindex + 1'b1;
-        hindex = 0;
+        vindex <= vindex + 1'b1;
+        hindex <= 0;
     end
     else
     begin
-        hindex = hindex + 1'b1;
+        hindex <= hindex + 1'b1;
     end
-    rgb = characterraster[{~vindex, ~hindex}] ? fgrgb : bgrgb;
+
+    if (blink && blink_timer[5])
+        rgb <= bgrgb;
+    else
+        rgb <= characterraster[{~vindex, ~hindex}] ? fgrgb : bgrgb;
 end
 endmodule
