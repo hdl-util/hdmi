fe4665bbc9769142e9bb52db29f2aa801002132b
Sameer Puri <sameer@users.noreply.github.com>
2020-01-13 11:34:42 -0600

    Swap priority of regen clock packet and audio sample packet, migrate to logic here and there

--- top/max10/max10_top.sv
+++ top/max10/max10_top.sv
@@ -3,17 +3,15 @@ module max10_top (
     input  wire       CLK_32KHZ,
 //    input  wire			RST,
 
-    output wire       	CLK_50MHZ_ENABLE,
-    output wire			CLK_32KHZ_ENABLE,
-    output wire	[7:0]	LED,
+    output wire       	CLK_50MHZ_ENABLE = 1'b1,
+    output wire			CLK_32KHZ_ENABLE = 1'b1,
+    output wire	[7:0]	LED = 8'd0,
 
     output wire [2:0] 	tmds_p,
     output wire			tmds_clock_p,
     output wire [2:0] 	tmds_n,
     output wire			tmds_clock_n
 );
-assign CLK_50MHZ_ENABLE = 1'b1;
-assign CLK_32KHZ_ENABLE = 1'b1;
 
 wire clk_tmds;
 wire clk_pixel;
@@ -27,8 +25,9 @@ logic [7:0] remaining;
 buffer #(.CHANNELS(1)) buffer (.clk_audio(CLK_32KHZ), .clk_pixel(clk_pixel), .packet_enable(packet_enable && packet_type == 8'd2), .audio_in('{audio_in}), .audio_out('{audio_out}), .remaining(remaining));
 
 logic [7:0] packet_type;
-assign packet_type = remaining > 0 ? 8'd2 : !audio_clock_regeneration_sent ? 8'd1 : 8'd0;
-reg audio_clock_regeneration_sent = 1'b0;
+assign packet_type = !audio_clock_regeneration_sent ? 8'd1 : remaining > 0 ? 8'd2 : 8'd0;
+
+logic audio_clock_regeneration_sent = 1'b0;
 always @(posedge clk_pixel)
 begin
     if (cx == 0 && cy == 0)
@@ -42,8 +41,8 @@ wire [9:0] cx, cy;
 wire packet_enable;
 hdmi #(.VIDEO_ID_CODE(3)) hdmi(.clk_tmds(clk_tmds), .clk_pixel(clk_pixel), .rgb(rgb), .audio_sample_word('{audio_out, audio_out}), .packet_type(packet_type), .tmds_p(tmds_p), .tmds_clock_p(tmds_clock_p), .tmds_n(tmds_n), .tmds_clock_n(tmds_clock_n), .cx(cx), .cy(cy), .packet_enable(packet_enable));
 
-reg [7:0] character = 8'h30;
-reg [5:0] prevcy = 0;
+logic [7:0] character = 8'h30;
+logic [5:0] prevcy = 6'd0;
 always @(posedge clk_pixel)
 begin
     if (cy == 10'd0)
