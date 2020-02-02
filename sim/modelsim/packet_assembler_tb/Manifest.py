303e750122f0e16f30ce062dfd25a7f2947ee955
Sameer Puri <sameer@users.noreply.github.com>
2020-02-01 22:04:57 -0600

    Remove outddated packet_assembler and hdmi_tb testbenches

--- sim/modelsim/packet_assembler_tb/Manifest.py
+++
@@ -1,9 +0,0 @@
-action = "simulation"
-sim_tool = "modelsim"
-sim_top = "packet_assembler_tb"
-
-sim_post_cmd = "vsim -novopt -do ../vsim.do -i packet_assembler_tb"
-
-modules = {
-  "local" : [ "../../../testbench/hdmi_tb" ],
-}
--- testbench/hdmi_tb/Manifest.py
+++
@@ -1,8 +0,0 @@
-files = [
-    "hdmi_tb.sv",
-    "packet_assembler_tb.sv"
-]
-
-modules = {
-  "local" : [ "../../modules/hdmi" ],
-}
