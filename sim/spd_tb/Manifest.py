945e48f7e63f233a54db21b2773d8eb059e38e4b
Sameer Puri <sameer@users.noreply.github.com>
2021-02-06 19:32:39 -0500

    swap from travis to github actions, update modelsim

--- sim/spd_tb/Manifest.py
+++ sim/spd_tb/Manifest.py
@@ -2,7 +2,7 @@ action = "simulation"
 sim_tool = "modelsim"
 sim_top = "spd_tb"
 
-sim_post_cmd = "vsim -novopt -do ../vsim.do -c spd_tb"
+sim_post_cmd = "vsim -do ../vsim.do -c spd_tb"
 
 modules = {
   "local" : [ "../../test/spd_tb" ],
